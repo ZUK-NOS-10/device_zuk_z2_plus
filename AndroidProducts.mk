ArrowOS-Devices/android_device_zuk_z2_plus

z2_plus: Add common lunch choices
Change-Id: Ib90fbce8bb3a188b4a92ba6d94ae03b71ed8c5e5

AndroidProducts.mk
@@ -15,3 +15,7 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/arrow_z2_plus.mk
+
+COMMON_LUNCH_CHOICES := \
+    arrow_z2_plus-userdebug \
+    arrow_z2_plus-eng

ZUK-NOS-10/device_zuk_z2_plus

AndroidProducts.mk
@@ -15,3 +15,7 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_z2_plus.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_z2_plus-userdebug \
+    lineage_z2_plus-eng